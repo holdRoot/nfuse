614587a470fff68a63ae2c3e00112dabf02f54d6
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2019-02-14 07:37:44 -0500

    7 37

--- src/lib/nfuse.cpp
+++ src/lib/nfuse.cpp
@@ -1,6 +1,37 @@
 #include "src/include/nfuseFS.h"
 
 #include <iostream>
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <sys/types.h>
+#include <dirent.h>
+
+typedef std::vector<std::string> stringvec;
+
+void read_directory(const std::string& name, stringvec& v)
+{
+    DIR* dirp = opendir(name.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != nullptr) {
+        v.push_back(dp->d_name);
+    }
+    closedir(dirp);
+}
+
+void
+list_dir(const std::string &dir)
+{
+    stringvec v;
+    read_directory(dir, v);
+
+    for (const auto &item : v) {
+        std::cout << item << std::endl;
+    }
+}
+
 
 int
 prep()
@@ -22,13 +53,23 @@ prep()
 int
 test_bench()
 {
-    std::cout << "Last stats: " << std::endl;
-    int res = 0;
-    auto fd = open("nfusestore", 0);
-    struct stat stat1{};
-    res = fstat(fd, &stat1);
-    std::cout << "Last modified: " << stat1.st_atim.tv_sec << std::endl;
-    return res;
+    std::ifstream file;
+    char cwd[FILENAME_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    auto dir = std::string(cwd);
+    list_dir("nfusestore");
+
+    auto my_file_name = dir + "/nfusestore/myfile";
+    std::cout << my_file_name << std::endl;
+    file.open(my_file_name);
+
+    std::string buff;
+    file >> buff;
+
+    std::cout << buff << std::endl;
+
+    return 0;
 }
 
 int
